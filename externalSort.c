7db7626b196232b55f2b22f722f88273f3657384
Felipe Estevanatto <69355764+FelipeEstevanatto@users.noreply.github.com>
2024-09-08 10:44:24 -0300

    start code

--- externalSort.c
+++ externalSort.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * External sort
  * 
@@ -26,25 +23,146 @@ Faça um relatório com gráficos que comparam o desempenho do método implement
 cada entrada. Descreva os detalhes dos experimentos realizados e discuta sobre os resultados
 observados. Para este trabalho, considere M = 5.000.
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define CHUNK_SIZE 5000
+
+void mergeSortedFiles(FILE **tempFiles, int numFiles, FILE *outputFile);
+void quicksort(int *array, int left, int right);
 
 int main() {
+    FILE *inputFile = fopen("../inputs/input1.txt", "r");
 
-    FILE *fptr;
-
-    fptr = fopen("../inputs/input1.txt", "r");
-
-    if (fptr == NULL) {
+    if (inputFile == NULL) {
         printf("Error! opening file");
         exit(1);
     }
 
-    // files are filled with random integers one by line
-    int num;
-    while (fscanf(fptr, "%d", &num) == 1) {
-        printf("%d\n", num);
+    int *buffer = (int *)malloc(CHUNK_SIZE * sizeof(int));
+    int count = 0, tempFileCount = 0;
+    char tempFileName[20];
+    FILE *tempFile;
+
+    // Read the file in chunks of CHUNK_SIZE
+    while (fscanf(inputFile, "%d", &buffer[count]) == 1) {
+        count++;
+        if (count == CHUNK_SIZE) {
+            quicksort(buffer, 0, count - 1);
+            sprintf(tempFileName, "temp%d.txt", tempFileCount++);
+            tempFile = fopen(tempFileName, "w");
+            for (int i = 0; i < count; i++) {
+                fprintf(tempFile, "%d\n", buffer[i]);
+            }
+            fclose(tempFile);
+            count = 0;
+        }
     }
 
+    if (count > 0) {
+        quicksort(buffer, 0, count - 1);
+        sprintf(tempFileName, "temp%d.txt", tempFileCount++);
+        tempFile = fopen(tempFileName, "w");
+        for (int i = 0; i < count; i++) {
+            fprintf(tempFile, "%d\n", buffer[i]);
+        }
+        fclose(tempFile);
+    }
 
+    free(buffer);
+    fclose(inputFile);
+
+    FILE **tempFiles = (FILE **)malloc(tempFileCount * sizeof(FILE *));
+    for (int i = 0; i < tempFileCount; i++) {
+        sprintf(tempFileName, "temp%d.txt", i);
+        tempFiles[i] = fopen(tempFileName, "r");
+    }
+
+    FILE *outputFile = fopen("sorted_output.txt", "w");
+    mergeSortedFiles(tempFiles, tempFileCount, outputFile);
+
+    for (int i = 0; i < tempFileCount; i++) {
+        fclose(tempFiles[i]);
+    }
+    free(tempFiles);
+    fclose(outputFile);
 
     return 0;
+}
+
+void mergeSortedFiles(FILE **tempFiles, int numFiles, FILE *outputFile) {
+    int *heap = (int *)malloc(numFiles * sizeof(int));
+    int *indices = (int *)malloc(numFiles * sizeof(int));
+    int i, minIndex, minValue;
+
+    for (i = 0; i < numFiles; i++) {
+        if (fscanf(tempFiles[i], "%d", &heap[i]) != 1) {
+            heap[i] = INT_MAX;
+        }
+        indices[i] = 0;
+    }
+
+    while (1) {
+        minIndex = -1;
+        minValue = INT_MAX;
+
+        for (i = 0; i < numFiles; i++) {
+            if (heap[i] < minValue) {
+                minValue = heap[i];
+                minIndex = i;
+            }
+        }
+
+        if (minValue == INT_MAX) {
+            break;
+        }
+
+        fprintf(outputFile, "%d\n", minValue);
+
+        if (fscanf(tempFiles[minIndex], "%d", &heap[minIndex]) != 1) {
+            heap[minIndex] = INT_MAX;
+        }
+    }
+
+    // Free the allocated memory
+    free(heap);
+    free(indices);
+
+    // Close the file handles before removing the files
+    for (i = 0; i < numFiles; i++) {
+        fclose(tempFiles[i]);
+    }
+
+    // Remove the temporary files
+    for (i = 0; i < numFiles; i++) {
+        char tempFileName[20];
+        sprintf(tempFileName, "temp%d.txt", i);
+        remove(tempFileName);
+    }
+}
+
+void quicksort(int *array, int left, int right) {
+    int i = left, j = right;
+    int tmp;
+    int pivot = array[(left + right) / 2];
+
+    while (i <= j) {
+        while (array[i] < pivot)
+            i++;
+        while (array[j] > pivot)
+            j--;
+        if (i <= j) {
+            tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+
+    if (left < j)
+        quicksort(array, left, j);
+    if (i < right)
+        quicksort(array, i, right);
 }
